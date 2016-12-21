f8edfcc35515a67ec8b61ee88a1a1ba7e6d5a7f3
Bihan Zhuang <bihan.zhuang@duke.edu>
2016-12-21 18:47:11 -0500

    add deploy

---
+++ flaskr.py
@@ -0,0 +1,17 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \render_template, flash
+
+# create application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE = os.path.join(app.root_path, 'flask.db'),
+    SECRET_KEY = 'development key',
+    USERNAME = 'admin',
+    PASSWORD = 'default'
+))
+app.config.from_envvar('FLASK_SETTINGS', silent=True)
